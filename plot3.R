2eb1892205dfd236961bb7542b77256067870a09
Venki Sundarrajan <venki.sundar@gmail.com>
2016-07-31 19:59:22 +0530

    Plot 3
    Plot 3 R Code Behind

---
+++ plot3.R
@@ -0,0 +1,13 @@
+> data <- read.table("D://Data Science Working Directory//household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2880)
+> data <- data %>% mutate(V1 = as.POSIXct(dmy_hms(as.character(paste(V1, V2)))),
++                         V7 = as.numeric(as.character(V7)),
++                         V8 = as.numeric(as.character(V8)),
++                         V9 = as.numeric(as.character(V9))) %>% select(V1,V7:V9)
+> 
+> 
+> with(data, plot(V1,V7, type="n", xlab = "", ylab = "Energy Sub Metering"))
+> with(data, points(V1,V7, col="black", type="l"))
+> with(data, points(V1,V8, col="red", type="l"))
+> with(data, points(V1,V9, col="blue", type="l"))
+> legend("topright", lty=1, col = c("black", "red", "blue"), 
++        legend = c("Sub_Metering_1", "Sub_Metering_2", "Sub_Metering_3"))
