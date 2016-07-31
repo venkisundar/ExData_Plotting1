1a1c9d96da9ea5004e16e8a6de63a58a66eed025
Venki Sundarrajan <venki.sundar@gmail.com>
2016-07-31 19:12:41 +0530

    Plot 1
    Plot 1 code behind R

---
+++ Plot1.R
@@ -0,0 +1,7 @@
+> # reading relevant data
+> data <- read.table("D://Data Science Working Directory//household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2880)
+> # clearing name row and unrelevant variables, creating numerics
+> data <- data %>% select(V3) %>% mutate(V3 = as.numeric(as.character(V3)))
+> # create histogram
+> hist(data$V3, col = "red", main = "Global Active Power",
++      xlab = "Global Active Power (killowatts)", ylab = "Frequency")
